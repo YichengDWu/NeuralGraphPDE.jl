7cef3c86fcbe35340dea1ae86cf960eea10b6fab
NeuralGraphPDE <yicheng.wu@ucalgary.ca>
2022-06-16 23:37:09 -0600

    fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,26 @@
 using NeuralGraphPDE
+using GraphNeuralNetworks
+using Random
+using Lux
 using Test
 
 @testset "layers" begin
     @testset "conv" begin
-        @test "gcn" begin
+        @testset "gcn" begin
             s = [1,1,2,3]
             t = [2,3,1,1]
             g = GNNGraph(s, t)
             x = randn(3, g.num_nodes)
             l = ExplicitGCNConv(3 => 5) 
 
-            # setup layer
             rng = Random.default_rng()
             Random.seed!(rng, 0)
 
             ps, st = Lux.setup(rng, l)
-
-            # forward pass
-            y = l(g, x, ps, st)   
+            @test st == (;)
+            y,st = l(g, x, ps, st)   
             @test size(y) == (5, g.num_nodes)
+            @test st == (;)
         end
     end
 end
