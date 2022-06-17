55b56ceffea8b0d717b7432eae4f3dba88bb5f44
NeuralGraphPDE <yicheng.wu@ucalgary.ca>
2022-06-16 23:09:38 -0600

    add test

--- src/NeuralGraphPDE.jl
+++ src/NeuralGraphPDE.jl
@@ -3,12 +3,12 @@ module NeuralGraphPDE
 using Lux, GraphNeuralNetworks, NNlib
 using Lux: AbstractExplicitContainerLayer, AbstractExplicitLayer, glorot_normal, glorot_uniform,
            ones32, zeros32
-
-import GraphNeuralNetworks: propagate, apply_edges, ADJMAT_T
-import Lux: initialparameters
+using GraphNeuralNetworks: ADJMAT_T
 using Random
 using Statistics: mean
 
+import GraphNeuralNetworks: propagate, apply_edges
+import Lux: initialparameters
 include("utils.jl")
 include("msgpass.jl")
 include("layers.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
 using NeuralGraphPDE
 using Test
 
-@testset "NeuralGraphPDE.jl" begin
-    @test 1<2
+@testset "layers" begin
+    @testset "conv" begin
+        @test "gcn" begin
+            s = [1,1,2,3]
+            t = [2,3,1,1]
+            g = GNNGraph(s, t)
+            x = randn(3, g.num_nodes)
+            l = ExplicitGCNConv(3 => 5) 
+
+            # setup layer
+            rng = Random.default_rng()
+            Random.seed!(rng, 0)
+
+            ps, st = Lux.setup(rng, l)
+
+            # forward pass
+            y = l(g, x, ps, st)   
+            @test size(y) == (5, g.num_nodes)
+        end
+    end
 end
